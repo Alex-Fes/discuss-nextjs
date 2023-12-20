8013aa0780c11d254523a7005a3969a8b5e19b10
Alex-Fes <aleksei.fesenko86@gmail.com>
2023-12-20 19:26:28 +0100

    add signIn, signOut buttons,

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,22 @@
 import { Button } from '@nextui-org/react'
+import * as actions from "@/src/actions";
+// import {auth} from "@/src/auth";
+import Profile from "@/src/components/profile";
 
-export default function Home() {
-  return (
-   <div>
-     <Button>Click me</Button>
-   </div>
-  )
+export default async function Home() {
+    // const session = await auth()
+
+    return (
+        <div>
+
+            <form action={actions.signIn}>
+                <Button type={'submit'}>Sign In</Button>
+            </form>
+            <form action={actions.signOut}>
+                <Button type={'submit'}>Sign Out</Button>
+            </form>
+
+            <Profile/>
+        </div>
+    )
 }
