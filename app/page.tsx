91afd308333db35d1152b8d163b8ce811eb800f2
Alex-Fes <aleksei.fesenko86@gmail.com>
2023-12-21 17:55:30 +0100

    create topic-create-form.tsx, add to main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,20 @@
-import { Button } from '@nextui-org/react'
+import {Button} from '@nextui-org/react'
 import * as actions from "@/src/actions";
 // import {auth} from "@/src/auth";
 import Profile from "@/src/components/profile";
+import TopicCreateForm from "@/src/components/topics/topic-create-form";
 
 export default async function Home() {
     // const session = await auth() // use in server component
 
     return (
-        <div>
-
-            <form action={actions.signIn}>
-                <Button type={'submit'}>Sign In</Button>
-            </form>
-            <form action={actions.signOut}>
-                <Button type={'submit'}>Sign Out</Button>
-            </form>
-
-            <Profile/>
+        <div className={'grid grid-cols-4 gap-4 p-4'}>
+            <div className={'col-span-3'}>
+                <h1 className={'text-xl m-2'}>Top posts</h1>
+            </div>
+            <div>
+                <TopicCreateForm/>
+            </div>
         </div>
     )
 }
