a93cb813c719dcc06c602b1a96e3dd932baac4af
Alex-Fes <aleksei.fesenko86@gmail.com>
2023-12-21 18:16:53 +0100

    add names to input and textarea, get it in action

--- src/actions/create-topic.ts
+++ src/actions/create-topic.ts
@@ -1,5 +1,8 @@
 'use server'
 
-export async function createTopic() {
+export async function createTopic(formData: FormData) {
     //todo: revalidate the homepage
+    const name = formData.get('name')
+    const description = formData.get('description')
+
 }
