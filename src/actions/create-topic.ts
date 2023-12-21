38c8d6b6c76344436f14ce700f514a72b2264fcd
Alex-Fes <aleksei.fesenko86@gmail.com>
2023-12-21 18:31:15 +0100

    add zod validation, create createTopicSchema, validation data from TopicCreateForm

--- src/actions/create-topic.ts
+++ src/actions/create-topic.ts
@@ -1,8 +1,18 @@
 'use server'
+import { z } from 'zod'
 
+const createTopicSchema = z.object({
+    name: z.string().min(3).regex(/^[a-z0-9-]+$/, {message: 'Must be lowercase letters, numbers, and dashes only'}),
+    description: z.string().min(10),
+})
 export async function createTopic(formData: FormData) {
     //todo: revalidate the homepage
-    const name = formData.get('name')
-    const description = formData.get('description')
+    const result = createTopicSchema.safeParse({
+        name: formData.get('name'),
+        description: formData.get('description'),
+    })
 
+    if (!result.success) {
+        console.log(result.error.flatten().fieldErrors)
+    }
 }
