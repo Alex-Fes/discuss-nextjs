51ee8859ad5beee7272de5eec1cb50e75fbd1cae
Alex-Fes <aleksei.fesenko86@gmail.com>
2023-12-21 11:17:34 +0100

    create separate files for server action

---
+++ src/actions/create-comment.ts
@@ -0,0 +1,5 @@
+'use server'
+
+export async function createComment(formState: { message: string }, formData: FormData) {
+
+}
---
+++ src/actions/create-topic.ts
@@ -0,0 +1,5 @@
+'use server'
+
+export async function createTopic(formState: { message: string }, formData: FormData) {
+
+}
