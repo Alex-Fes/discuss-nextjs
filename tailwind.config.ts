94548fd3ddab791ba2cacd03b5e517e4c1d4a9cc
Alex-Fes <aleksei.fesenko86@gmail.com>
2023-12-21 20:46:41 +0100

    fix styles

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,12 +1,12 @@
-import type {Config} from 'tailwindcss'
-import {nextui} from '@nextui-org/react'
+import type { Config } from 'tailwindcss';
+import { nextui } from '@nextui-org/react';
 
 const config: Config = {
     content: [
-        './pages/**/*.{js,ts,jsx,tsx,mdx}',
-        './components/**/*.{js,ts,jsx,tsx,mdx}',
-        './app/**/*.{js,ts,jsx,tsx,mdx}',
-        './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx,mdx}',
+        './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+        './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+        './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+        './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}',
     ],
     theme: {
         extend: {
@@ -19,5 +19,5 @@ const config: Config = {
     },
     darkMode: 'class',
     plugins: [nextui()],
-}
-export default config
+};
+export default config;
