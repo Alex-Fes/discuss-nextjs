81b3a224d5675700f009df13a6d7ddca912f6e01
Alex-Fes <aleksei.fesenko86@gmail.com>
2023-12-23 17:47:24 +0100

    add PostList with fetchPostsBySearchTerm

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,4 +1,6 @@
 import {redirect} from "next/navigation";
+import {fetchPostsBySearchTerm} from "@/src/db/queries/posts";
+import PostList from "@/src/components/posts/post-list";
 
 interface SearchPageProps {
     searchParams: {
@@ -10,12 +12,15 @@ export default async function SearchPage({searchParams}: SearchPageProps) {
     const {term} = searchParams
 
     if (!term) {
-       redirect('/')
+        redirect('/')
     }
 
     return (
         <div>
             <h1>Search results for {term}</h1>
+            <div>
+                <PostList fetchData={() => fetchPostsBySearchTerm(term)}/>
+            </div>
         </div>
     )
 
