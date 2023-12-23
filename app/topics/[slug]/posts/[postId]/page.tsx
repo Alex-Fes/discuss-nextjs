724a96e974454c924979f8a5257f0bb315af739a
Alex-Fes <aleksei.fesenko86@gmail.com>
2023-12-23 16:45:59 +0100

    add PostShowLoading to Suspense

--- app/topics/[slug]/posts/[postId]/page.tsx
+++ app/topics/[slug]/posts/[postId]/page.tsx
@@ -4,6 +4,7 @@ import CommentList from "@/src/components/comments/comment-list";
 import CommentCreateForm from "@/src/components/comments/comment-create-form";
 import paths from "@/src/paths";
 import {Suspense} from "react";
+import PostShowLoading from "@/src/components/posts/post-show-loading";
 
 interface PostShowPageProps {
     params: {
@@ -20,7 +21,7 @@ export default async function PostShowPage({params}: PostShowPageProps) {
             <Link className="underline decoration-solid" href={paths.topicShow(slug)}>
                 {"< "}Back to {slug}
             </Link>
-            <Suspense fallback={<div>Loading...</div>}>
+            <Suspense fallback={<PostShowLoading/>}>
                 <PostShow postId={postId}/>
             </Suspense>
             <CommentCreateForm postId={postId} startOpen/>
