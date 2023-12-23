2f9071c2b80a9f56bfa88eb734e4c3c43cecd6df
Alex-Fes <aleksei.fesenko86@gmail.com>
2023-12-23 17:47:52 +0100

    create fetchPostsBySearchTerm for fetching searching posts

--- src/db/queries/posts.ts
+++ src/db/queries/posts.ts
@@ -1,6 +1,7 @@
 import {Post} from "@prisma/client";
 import {db} from "@/src/db";
 
+// export type PostWithData = Awaited<ReturnType<typeof fetchPostByTopicSlug>>[number]
 
 export type PostWithData = Post & {
     topic: { slug: string }
@@ -8,8 +9,22 @@ export type PostWithData = Post & {
     _count: { comments: number }
 }
 
-// export type PostWithData = Awaited<ReturnType<typeof fetchPostByTopicSlug>>[number]
+export function fetchPostsBySearchTerm (term: string): Promise<PostWithData[]> {
+    return db.post.findMany({
+        include: {
+            topic: {select: {slug: true}},
+            user: {select: {name: true, image: true}},
+            _count: {select: {comments: true}},
+        },
+        where: {
+            OR: [
+                {title: {contains: term}},
+                {content: {contains: term}}
+            ]
+        }
+    })
 
+}
 
 export function fetchPostByTopicSlug(slug: string): Promise<PostWithData[]> {
     return db.post.findMany({
